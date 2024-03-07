e9187a5478c3d79dbc84668f57270c0f8a5dee34
rado-kolev <rado.k.kolev@gmail.com>
2024-03-07 23:06:02 +0000

    Add components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,69 @@
 import styled from 'styled-components';
 import ImgSlider from './ImgSlider';
+import Viewers from './Viewers';
+import Recommends from './Recommends';
+import NewDisney from './NewDisney';
+import Originals from './Originals';
+import Trending from './Trending';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import db from '../firebase';
+import { setMovies } from '../features/movie/movieSlice';
+import { selectUserName } from '../features/user/userSlice';
+import { collection, onSnapshot } from 'firebase/firestore';
 
 const Home = (props) => {
+  const dispatch = useDispatch();
+  const username = useSelector(selectUserName);
+  let recommend = [];
+  let newDisney = [];
+  let original = [];
+  let trending = [];
+
+  const colRef = collection(db, 'movies');
+
+  useEffect(() => {
+    onSnapshot(colRef, (snapshot) => {
+      snapshot.docs.map((doc) => {
+        switch (doc.data().type) {
+          case 'recommend':
+            recommend = [...recommend, { id: doc.id, ...doc.data() }];
+            break;
+
+          case 'new':
+            newDisney = [...newDisney, { id: doc.id, ...doc.data() }];
+            break;
+
+          case 'original':
+            original = [...original, { id: doc.id, ...doc.data() }];
+            break;
+
+          case 'trending':
+            trending = [...trending, { id: doc.id, ...doc.data() }];
+            break;
+        }
+      });
+
+      dispatch(
+        setMovies({
+          recommend: recommend,
+          newDisney: newDisney,
+          original: original,
+          trending: trending,
+        })
+      );
+    });
+  }, [username]);
+  
+
   return (
     <Container>
       <ImgSlider />
+      <Viewers />
+      <Recommends />
+      <NewDisney />
+      <Originals />
+      <Trending />
     </Container>
   )
 }
